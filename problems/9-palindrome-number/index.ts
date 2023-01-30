6957f7ca47fd76fb2932a22a1d6dae770251e749
ytakhs <21127627+ytakhs@users.noreply.github.com>
2023-01-31 00:42:34 +0900

    Modify isPalindrome

--- problems/9-palindrome-number/index.ts
+++ problems/9-palindrome-number/index.ts
@@ -6,10 +6,10 @@ export function isPalindrome(x: number): boolean {
   let c = x;
   let val = 0;
 
-  while (c > 0) {
+  while (c > val) {
     val = val * 10 + (c % 10);
     c = Math.floor(c / 10);
   }
 
-  return val === x;
+  return c === val || c === Math.floor(val / 10);
 }
